84367c520982556a6855cf8f17878dadc32d0487
Abby Aldridge <abiald@amazon.com>
2022-02-01 13:49:58 +0000

    Adding basic add-question page

--- src/components/Quiz-Manager.tsx
+++ src/components/Quiz-Manager.tsx
@@ -1,9 +1,10 @@
 import { Login } from "./login/Login";
 import { Quizzes } from "./quizzes/Quizzes";
 import { Questions } from "./questions/Questions-Admin";
+import {AddQuestion} from "./add-question/AddQuestion"
 export function QuizManager() {
     return (
-        <Questions/>
+        <AddQuestion/>
     )
 }
 
